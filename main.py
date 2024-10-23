90f708b81c25e8723722e76fac3f49b864f4d4f1
Aara-A06 <junminhan20@gmail.com>
2024-10-23 18:56:05 -0400

    Changed/modified Kevin's repository- Aara Ahn :)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+#Lab 6 Group 45
+#This is Kevin's code
+#Aara Ahn modified this
+
 def encode(password):
-    encoded_pass = ''
+    encoded_pass = ""
     for digit in password:
         # Shift each digit up by 3 numbers
         new_digit = (int(digit) + 3) % 10
@@ -8,7 +12,7 @@ def encode(password):
 
 
 def decode(encoded_pass):
-    decoded_pass = ''
+    decoded_pass = ""
     for digit in encoded_pass:
         # Shift each digit down by 3 numbers
         new_digit = (int(digit) - 3) % 10
@@ -17,16 +21,15 @@ def decode(encoded_pass):
 
 
 def main():
-    encoded_password = ''
-
+    encoded_password = ""
+    #Dislplay menu options
     while True:
-        print("Menu")
-        print("-------------")
+        print("\nMenu\n-------------")
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
 
-        option = input("Please enter an option: ")
+        option = input("Please enter an option: ")  #Allows the user to input options from the menu
 
         if option == '1':
             password = input("Please enter your password to encode: ")
@@ -42,10 +45,10 @@ def main():
 
         elif option == '3':
             break
-
+        #"Exiting the program" statement is not needed, great catch!
         else:
             print("Invalid option. Please try again.")
-
+#Kevin did a great job!
 
 if __name__ == "__main__":
     main()
